3ecafdb21b5b39bccec3c50528b3f799700afd32
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-10-30 13:42:18 +0600

    Setup complete using Shadcn UI

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+// import Footer from "@/Shared/Footer";
+// import Navbar from "@/Shared/Navbar";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+
+    return (
+        <div>
+            {/* <Navbar></Navbar> */}
+            <Outlet></Outlet>
+            {/* <Footer></Footer> */}
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,28 @@
+
+import MainLayout from "@/Layouts/MainLayout";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <h1>Hiii</h1>
+      },
+    //   {
+    //     path: "about",
+    //     element: <AboutPage></AboutPage>,
+    //   },
+    //   {
+    //     path: "services",
+    //     element: <ServicesPage></ServicesPage>,
+    //   },
+    //   {
+    //     path: "contact",
+    //     element: <ContactPage></ContactPage>,
+    //   },
+    ],
+  },
+]);
